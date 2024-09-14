936afdbefe9bb604fc1eaa685093089ee41b6bb8
Fahad Ali Babar <rajafahadali931@gmail.com>
2024-09-14 13:18:54 +0500

    com

--- Backend/index.js
+++ Backend/index.js
@@ -8,8 +8,26 @@
         
 //     }
 // })
-const http=require("http")
-const data=http.createServer((req,res)=>{
-res.end("hello world")
+// const http=require("http")
+// const data=http.createServer((req,res)=>{
+// res.end("hello world")
+// })
+// data.listen(5000)
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+// app.use(function(req,res,next){// this is middleware
+//     console.log("middleware executed");
+//     next();//after executing middleware the next command will forward the request to routes
+    
+// })
+
+app.get('/', function (req, res) {
+  res.send("helloooo fahad")
 })
-data.listen(5000)
+app.get("/profile", function(req,res){
+res.send("i am 21 years o")
+})
+
+app.listen(3000)
