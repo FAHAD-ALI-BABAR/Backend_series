efc6eccb1817c3334e58fc863ea3c66ecf16b47d
Fahad Ali Babar <rajafahadali931@gmail.com>
2024-09-19 14:19:36 +0500

    comm

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}))//to make datat readable
 // app.use(function(req,res,next){// this is middleware
 //     console.log("middleware executed");
 //     next();//after executing middleware the next command will forward the request to routes
